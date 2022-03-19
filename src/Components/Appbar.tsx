82bacd4c62a6f043e096dd49a5027f91a849769b
kaleemku9991 <kaleemku9991@gmail.com>
2022-03-20 01:31:29 +0500

    appBar

--- src/Components/Appbar.tsx
+++ src/Components/Appbar.tsx
@@ -10,7 +10,7 @@ function Appbar() {
   <input type="checkbox" id="nav-check"/>
   <div className="nav-header">
     <div className="nav-title">
-      JoGeek
+      ssk
     </div>
   </div>
   <div className="nav-btn">
@@ -22,11 +22,11 @@ function Appbar() {
   </div>
   
   <div className="nav-links">
-    <a href="//github.io/jo_geek" target="_blank">Github</a>
-    <a href="http://stackoverflow.com/users/4084003/" target="_blank">Stackoverflow</a>
-    <a href="https://in.linkedin.com/in/jonesvinothjoseph" target="_blank">LinkedIn</a>
-    <a href="https://codepen.io/jo_Geek/" target="_blank">Codepen</a>
-    <a href="https://jsfiddle.net/user/jo_Geek/" target="_blank">JsFiddle</a>
+    <a href="//github.io" target="_blank">Github</a>
+    <a href="http://stackoverflow.com" target="_blank">Stackoverflow</a>
+    <a href="https://in.linkedin.com" target="_blank">LinkedIn</a>
+    <a href="https://codepen.io" target="_blank">Codepen</a>
+    <a href="https://jsfiddle.net" target="_blank">JsFiddle</a>
   </div>
 </div>
     </div>
